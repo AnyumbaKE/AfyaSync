d924c19f9da0ecae30f1bdfbefa24553ef2ec364
AnyumbaKE <esanyumba@gmail.com>
2024-08-18 15:17:00 +0300

    authentication handling

---
+++ frontend/auth/src/app/api/apiSlice.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { setCredentials, logOut } from "../../features/auth/authSlice";
+
+const api = process.env.REACT_APP_API_URL;
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: api,
+    credentials: "include",
+    prepareHeaders: (headers, { getState }) => {
+        return headers;
+    },
+})
+
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+    let result = await baseQuery(args, api, extraOptions);
+
+    if (result?.error) api.dispatch(logOut());
+    return result;
+};
+
+export const apiSlice = createApi({
+    baseQuery: baseQueryWithReauth,
+    endpoints: builder => ({})
+});
