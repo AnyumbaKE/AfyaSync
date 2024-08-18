eb8fcd618dea9f1d5b44d57ca0a4a4844ff00008
AnyumbaKE <esanyumba@gmail.com>
2024-08-18 17:46:10 +0300

    redirections

--- frontend/auth/src/App.js
+++ frontend/auth/src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Public from './components/Public';
+import Login from './features/auth/Login';
 
+/**
+ * Main application component that sets up routing for the application.
+ * This component uses React Router to define public and private routes.
+ */
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route index element={<Login />} />
+        {/* pulic routes*/}
+        
+        {/* private routes*/}
+    </Routes>
   );
 }
 
