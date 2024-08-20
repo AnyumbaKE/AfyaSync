faa50543e6713acf0b7f691e9fad8eb1a29eac8b
AnyumbaKE <esanyumba@gmail.com>
2024-08-20 19:16:19 +0300

    vaccine model

--- backend/models/vaccine.py
+++ backend/models/vaccine.py
@@ -9,5 +9,22 @@ class Vaccine(BM, Base):
     
     administeredForId = Column(
         String(40), ForeignKey("patients.id")
-    )
-    # ID of the patient receiving the vaccine
+    ) # ID of the patient receiving the vaccine
+    
+    administeredFor = relationship(
+        "Patient", back_populates="vaccines"
+    ) # relationship with the patient model
+    
+    administeredById = Column(
+        String(40), ForeignKey("hcws.id")
+    ) # ID of the HCW administering the vaccine
+    
+    administeredBy = relationship("HCW") # relationship with the HCW model
+    drugId = Column(
+        String(60), ForeignKey("drugs.id")
+    ) # ID of the drug used for the vaccine
+    
+    drug = relationship("Drug", uselist=True) # relationship with the drug model
+    
+    notes = Column(String(2048)) # Additional notes about the vaccine administration
+    
