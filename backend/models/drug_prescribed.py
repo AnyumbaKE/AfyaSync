937417a3ef7479949a83182ff24461ee0a0a3859
AnyumbaKE <esanyumba@gmail.com>
2024-08-22 16:37:45 +0300

    prescribed drugs in a prescription

---
+++ backend/models/drug_prescribed.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
+from models.base import BM, Base
+
+
+class DrugPrescribed(BM, Base):
+
+    __tablename__ = "drugsPrescribed"
+
+    drugId = Column(String(60), ForeignKey("drugs.id"))
+    drug = relationship("Drug", uselist=False)  # Define relationship with Drug class
+
+    prescriptionId = Column(String(40), ForeignKey("prescriptions.id"))
+    prescription = relationship(
+        "Prescription", back_populates="drugs"
+    )  # Define relationship with Prescription class
+
+    instructions = Column(String(2048))  # Instructions for taking the prescribed drug
